ad4debacea6656f06c26e88d5aeabef3f271513e
André Girol <andregirol@gmail.com>
2015-07-13 17:57:21 -0300

    Adicionando arquivos de configuração

---
+++ www/includes/config-exemplo.php
@@ -0,0 +1,13 @@
+<?php
+# Configurações iniciais para conexão com o banco
+
+    # Endereço do banco de dados
+    $dbaddress='localhost';
+    # Usuário do Banco de dados
+	$dbuser='mysql-user';
+    # senha do banco de dados
+	$dbpass='password';
+    # Nome do banco de dados a ser utilizado
+	$dbname='my-database';
+
+?>
