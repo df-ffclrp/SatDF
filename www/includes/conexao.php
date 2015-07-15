6d02b6d0f3225887598d6817b14d6d3418a700ba
André Girol <andregirol@gmail.com>
2015-07-15 12:34:51 -0300

    Funções passadas para mysqli
    Adicionada também pequena alteração na interface

--- www/includes/conexao.php
+++ www/includes/conexao.php
@@ -1,26 +1,27 @@
 <?php
 # configuracao de conexao com o banco de dados
 
-function conecta() {
+function conectaDB() {
 
     # Adiciona os parâmetros da conexão
     require('config.php');
 
 	# Prepara a conexao
-	$connect = mysql_connect($dbaddress, $dbuser, $dbpass);
+	$connect = mysqli_connect($dbaddress, $dbuser, $dbpass) or die ("Não foi possível conectar ao banco de dados");
 
     # Configura as transações para UTF-8
- 	mysql_set_charset('utf8', $connect);
+ 	mysqli_set_charset($connect, 'utf8');
 
 	# Caso a conexão seja reprovada, exibe na tela uma mensagem de erro
 	if (!$connect) {
 		echo "<h1>Falha na conexao com o Banco de Dados!</h1>";
-		mysql_error();
+		mysqli_error($connect);
  	}
 	 else {
 	# Caso a conexão seja aprovada, então conecta o Banco de Dados.
-	$db = mysql_select_db($dbname) or die (mysql_error());
+	$db = mysqli_select_db($connect,$dbname) or die ("Não foi possível selecionar o banco ." . mysqli_error($connect));
 	}
+    return $connect;
 }
 ?>
 
