06cbc2b58368c1be9924bd27c6d1d863bbfda2b3
André Luiz Girol <andregirol@gmail.com>
2017-09-19 16:27:11 -0300

    Add sample email config

---
+++ sat/includes/config_email_sample.php
@@ -0,0 +1,9 @@
+<?php
+
+# Configurações para envio de email:
+
+$hostname = 'smtp.host.com';
+$username = 'my_email@host.com';
+$password = 'my_pass';
+$from = 'my_email@host.com';
+$fromName = 'My Real Name';
