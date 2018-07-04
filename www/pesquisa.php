921f32b19cb678820f19029f4770840b97c51f04
André Luiz Girol <andregirol@gmail.com>
2018-07-04 18:32:07 -0300

    Link para Grupo de Física e Estatística

--- www/pesquisa.php
+++ www/pesquisa.php
@@ -24,6 +24,7 @@ include_once("includes/header.php");
 				<li><a href="http://www.biofisicamolecular.com.br/" target="_blank">Laboratório de Biofísica Molecular (LBM)</a></li>
 				<li><a href="http://dcm.ffclrp.usp.br/fotobiofisica/index.html" target="_blank">Fotobiofísica</a></li>
 				<li><a href="http://dgp.cnpq.br/dgp/espelhogrupo/7548264580175587" target="_blank">Fisica Radiológica e Dosimetria</a></li>
+				<li><a href="http://fisestat.wordpress.com " target="_blank">Grupo de Física Estatística Teórica e Computacional</a></li>
 			</ul>		
 	</div>
 
